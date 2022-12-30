9a57007cfa75119205a682c18c58cdc2cffd0d7e
unless <jbpv@163.com>
2022-12-30 23:16:21 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -66,8 +66,8 @@ sed -i "s/make -d/make/ig" /home/runner/work/build-actions/build-actions/openwrt
 # 可选内核 （5.4） （5.10） （5.15） （5.19） （6.0）
 
 cat >"$AMLOGIC_SH_PATH" <<-EOF
-amlogic_model=s905x3_rk3328
-amlogic_kernel=5.10.161_6.1.1
+amlogic_model=s905x3
+amlogic_kernel=6.1.1
 auto_kernel=true
 rootfs_size=960
 EOF
