8014e0d46f9ead27888f336c079ef80ceedebb77
unless <jbpv@163.com>
2023-04-21 16:32:12 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -5,7 +5,7 @@
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
 sed -i 's/+luci-i18n-base-zh_Hans//g' package/emortal/default-settings/Makefile
-
+cat package/emortal/default-settings/Makefile
 # 后台IP设置
 export Ipv4_ipaddr="192.168.111.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
