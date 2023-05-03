402a4480e386df99cd2507f685584e688478dbbe
unless <jbpv@163.com>
2023-05-03 09:33:12 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -3,8 +3,8 @@
 # DIY扩展二合一了，在此处可以增加插件
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
-sed -i 's/+luci-i18n-base-zh-cn//g' package/emortal/default-settings/Makefile
-cat package/emortal/default-settings/Makefile
+#sed -i 's/+luci-i18n-base-zh-cn//g' package/emortal/default-settings/Makefile
+#cat package/emortal/default-settings/Makefile
 # 后台IP设置
 export Ipv4_ipaddr="192.168.111.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
